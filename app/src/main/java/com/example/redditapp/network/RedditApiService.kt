c5be64cf58185953c95a4adc63aca8b0ed600687
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-09-09 09:41:09 +0300

    added api service with request

---
+++ app/src/main/java/com/example/redditapp/network/RedditApiService.kt
@@ -0,0 +1,11 @@
+package com.example.redditapp.network
+
+import com.example.redditapp.model.RedditResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RedditApiService {
+    @GET("/r/androiddev/top.json")
+    fun getTopPosts(@Query("limit") limit: Int) : Call<RedditResponse>
+}
