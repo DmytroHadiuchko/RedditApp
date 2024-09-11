e8c48715feb71ca8f12d9eeaec1cebd7a492912e
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-09-11 16:14:16 +0300

    added annotation for all fields

--- app/src/main/java/com/example/redditapp/model/RedditPost.kt
+++ app/src/main/java/com/example/redditapp/model/RedditPost.kt
@@ -3,8 +3,8 @@ package com.example.redditapp.model
 import com.google.gson.annotations.SerializedName
 
 data class RedditPost(@SerializedName("title") val title: String,
-                      val author: String,
-                      val url: String,
-                      val thumbnail:String?,
-                      val created_utc: Long,
-                      val num_comments: Int)
+                      @SerializedName("author") val author: String,
+                      @SerializedName("url") val url: String,
+                      @SerializedName("thumbnail") val thumbnail:String?,
+                      @SerializedName("created_utc") val created_utc: Long,
+                      @SerializedName("num_comments") val num_comments: Int)
